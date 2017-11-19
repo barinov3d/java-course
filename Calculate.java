2295b6071faf727dc150b11728e4a75591ce006f
barinov3d <Beketova4215>
2017-11-19 11:56:42 +0300

    Create calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,16 @@
 public class Calculate {
 	public static void main(String [] args){
-		System.out.println("Calculate...");
+System.out.println("Welcome to simple calculator!");
+		float first = Float.valueOf(args[0]);
+		float second = Float.valueOf(args[1]);
+		float sum = first + second;
+		System.out.println(first + " + " + second + " = " + sum );
+		float sub = first - second;
+		System.out.println(first + " - " + second + " = " + sub );
+		float div = first / second;
+		System.out.println(first + " / " + second + " = " + div );
+		float mult = first * second;
+		System.out.println(first + " * " + second + " = " + mult );
+
 	}
 }
